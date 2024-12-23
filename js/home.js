54dc6e29dfad3c101c3eb01e4855cf36a60f920d
atmabdullaham <atmabdullaham@gmail.com>
2024-12-23 12:33:15 +0600

    added cash out button and functionality to this button

--- js/home.js
+++ js/home.js
@@ -5,16 +5,50 @@
 
 document.getElementById("add-Money-con").addEventListener("click", function (event) {
  event.preventDefault();
- const addMoney = parseFloat(document.getElementById("amount").value);
+ const money = parseFloat(document.getElementById("amount").value);
 
  const pin = document.getElementById('account-pin').value;
- console.log(addMoney, pin)
+ console.log(money, pin)
 
  if (pin === "123") {
   const balance = parseFloat(document.getElementById("account-balance").innerText);
-  const newBalance = balance + addMoney;
+  const newBalance = balance + money;
   document.getElementById("account-balance").innerText = newBalance;
   console.log(newBalance)
+ } else {
+  console.log("Wrong Pin")
+ }
+
+})
+
+// 
+document.getElementById('add-money').addEventListener("click", function () {
+ document.getElementById("form-container").classList.remove("hidden")
+ document.getElementById("add-Money-con").classList.remove("hidden")
+ document.getElementById("cashout-con").classList.add("hidden")
+})
+//
+document.getElementById('cash-out').addEventListener("click", function () {
+ document.getElementById("form-container").classList.remove("hidden")
+ document.getElementById("cashout-con").classList.remove("hidden")
+ document.getElementById("add-Money-con").classList.add("hidden")
+})
+
+
+// Cash out confirmation button
+
+document.getElementById("cashout-con").addEventListener("click", function (event) {
+ event.preventDefault();
+ const money = parseFloat(document.getElementById("amount").value);
+
+ const pin = document.getElementById('account-pin').value;
+
+
+ if (pin === "123") {
+  const balance = parseFloat(document.getElementById("account-balance").innerText);
+  const newBalance = balance - money;
+  document.getElementById("account-balance").innerText = newBalance;
+
  } else {
   console.log("Wrong Pin")
  }
