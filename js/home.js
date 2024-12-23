2f97d575a6d166044deeec9506df479d41cba032
atmabdullaham <atmabdullaham@gmail.com>
2024-12-23 11:02:29 +0600

    added eventhandler to addmoney button and get the input value of money and pin

--- js/home.js
+++ js/home.js
@@ -1,6 +1,13 @@
+/**
+ * step-1: Add event listener and prevent reload after form submit
+ */
+
 
 document.getElementById("add-Money-con").addEventListener("click", function (event) {
  event.preventDefault();
- console.log("add money confirmation button clicked")
+ const addMoney = document.getElementById("amount").value;
+
+ const pin = document.getElementById('account-pin').value;
+ console.log(addMoney, pin)
 
 })
