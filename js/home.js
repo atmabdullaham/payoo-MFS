22fa62e588c46d1e5f0f95b6aa0d052f4b4c595c
atmabdullaham <atmabdullaham@gmail.com>
2024-12-23 11:38:10 +0600

    adde the money and show the new balance

--- js/home.js
+++ js/home.js
@@ -5,9 +5,18 @@
 
 document.getElementById("add-Money-con").addEventListener("click", function (event) {
  event.preventDefault();
- const addMoney = document.getElementById("amount").value;
+ const addMoney = parseFloat(document.getElementById("amount").value);
 
  const pin = document.getElementById('account-pin').value;
  console.log(addMoney, pin)
+
+ if (pin === "123") {
+  const balance = parseFloat(document.getElementById("account-balance").innerText);
+  const newBalance = balance + addMoney;
+  document.getElementById("account-balance").innerText = newBalance;
+  console.log(newBalance)
+ } else {
+  console.log("Wrong Pin")
+ }
 
 })
